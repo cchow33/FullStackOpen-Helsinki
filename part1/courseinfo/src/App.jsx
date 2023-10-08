a3f4ca0b797c77db7e3ae3c9ee8d3099cc178274
Carmen Chow <carmendevelops@gmail.com>
2023-10-08 16:55:41 -0400

    exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,48 @@
+// Refactoring with object literals:
+
 const App = () => {
   const course = "Half stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  const Header = (props) => {
+    return <h1>{props.course}</h1>;
   };
 
-  const sum = part1.exercises + part2.exercises + part3.exercises;
+  const Content = (props) => {
+    return (
+      <div>
+        {props.parts.map((part, index) => (
+          <div key={index}>
+            <p>{part.name + " " + part.exercises}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const Total = (props) => {
+    const sum = props.parts.reduce((a, b) => a + b.exercises, 0);
+    return <p>Total exercises: {sum}</p>;
+  };
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1.name + " " + part1.exercises}</p>
-      <p>{part2.name + " " + part2.exercises}</p>
-      <p>{part3.name + " " + part3.exercises}</p>
-      <p>Number of exercises {" " + sum}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
