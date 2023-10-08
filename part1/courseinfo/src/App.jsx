26d3232ef088eb9c1ba3840c45fd6a4368bfcfe9
Carmen Chow <carmendevelops@gmail.com>
2023-10-08 19:47:52 -0400

    exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,48 +1,28 @@
-// Refactoring with object literals:
+// Refactor the course and its parts into a single object:
 
 const App = () => {
-  const course = "Half stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-
-  const Header = (props) => {
-    return <h1>{props.course}</h1>;
-  };
-
-  const Content = (props) => {
-    return (
-      <div>
-        {props.parts.map((part, index) => (
-          <div key={index}>
-            <p>{part.name + " " + part.exercises}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  const Total = (props) => {
-    const sum = props.parts.reduce((a, b) => a + b.exercises, 0);
-    return <p>Total exercises: {sum}</p>;
+  const course = {
+    name: "Half stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
   };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h1>{course.name}</h1>
+      <div>
+        {course.parts.map((part, index) => (
+          <div key={index}>
+            <p>{part.name + ":" + " " + part.exercises}</p>
+          </div>
+        ))}
+      </div>
+      <p>
+        Total exercises: {course.parts.reduce((a, b) => a + b.exercises, 0)}
+      </p>
     </div>
   );
 };
