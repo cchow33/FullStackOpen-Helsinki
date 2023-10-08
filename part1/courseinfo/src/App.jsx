2f26859802a1bd06ce5f026255b4ab3ea18d2a26
Carmen Chow <carmendevelops@gmail.com>
2023-10-08 05:51:48 -0400

    1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,16 +1,54 @@
-function App() {
-  const Header = () => {};
-  const Content = () => {};
-  const Total = () => {};
+const App = () => {
+  const parts = [
+    "Fundamentals of React",
+    "Using props to pass data",
+    "State of a component",
+  ];
+  const course = "Half stack application development";
+  const exercises = [10, 7, 14];
 
-  const course = [];
+  const Header = () => {
+    return (
+      <>
+        <h1>{course}</h1>
+      </>
+    );
+  };
+
+  const Content = () => {
+    return (
+      <div>
+        <p>
+          {parts[0]}
+          {exercises[0]}
+        </p>
+        <p>
+          {parts[1]}
+          {exercises[1]}
+        </p>
+        <p>
+          {parts[2]}
+          {exercises[2]}
+        </p>
+      </div>
+    );
+  };
+
+  const Total = () => {
+    return <>{exercises[0] + exercises[1] + exercises[2]}</>;
+  };
+
+  console.log(course);
+
   return (
     <div>
       <Header course={course} />
       <Content />
-      <Total />
+      <p>
+        Number of exercises <Total />
+      </p>
     </div>
   );
-}
+};
 
 export default App;
