52b5e55ba8355e99a8642c340189decdef3a541d
Carmen Chow <carmendevelops@gmail.com>
2023-10-08 00:12:06 -0400

    exercise 1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  const Header = () => {};
+  const Content = () => {};
+  const Total = () => {};
+
+  const course = [];
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
+    </div>
+  );
+}
+
+export default App;
