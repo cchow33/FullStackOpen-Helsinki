81cf0100a17dc6548ffbaa79fab2fb542829ae42
Carmen Chow <carmendevelops@gmail.com>
2023-10-09 19:22:23 -0400

    part 5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,30 +1,13 @@
-// Refactor the course and its parts into a single object:
+import useState from "react";
 
 const App = () => {
-  const course = {
-    name: "Half stack application development",
-    parts: [
-      { name: "Fundamentals of React", exercises: 10 },
-      { name: "Using props to pass data", exercises: 7 },
-      { name: "State of a component", exercises: 14 },
-    ],
-  };
+  const [counter, setCounter] = useState(0);
 
-  return (
-    <div>
-      <h1>{course.name}</h1>
-      <div>
-        {course.parts.map((part, index) => (
-          <div key={index}>
-            <p>{part.name + ":" + " " + part.exercises}</p>
-          </div>
-        ))}
-      </div>
-      <p>
-        Total exercises: {course.parts.reduce((a, b) => a + b.exercises, 0)}
-      </p>
-    </div>
-  );
+  setTimeout(() => setCounter(counter + 1), 1000);
+
+  console.log("rendering...", counter);
+
+  return <div>{counter}</div>;
 };
 
 export default App;
